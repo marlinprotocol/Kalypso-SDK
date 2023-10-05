431b257e5e9fdd780ee4b44cd501d76c56597658
Akshay Meher <akshay_111meher@hotmail.com>
2023-10-05 21:35:28 +0400

    add read key

--- src/admin.ts
+++ src/admin.ts
@@ -1,13 +1,15 @@
 import { AbstractSigner, BytesLike, ContractTransactionResponse, Overrides } from "ethers";
-import { ProofMarketPlace__factory, ProofMarketPlace } from "./generated/typechain-types";
+import { ProofMarketPlace__factory, ProofMarketPlace, RsaRegistry, RsaRegistry__factory } from "./generated/typechain-types";
 
 export class Admin {
   private signer: AbstractSigner;
   private proofMarketPlace: ProofMarketPlace;
+  private rsaRegistry: RsaRegistry;
 
-  constructor(signer: AbstractSigner, proofMarketPlaceAddress: string) {
+  constructor(signer: AbstractSigner, proofMarketPlaceAddress: string, rsaRegistry: string) {
     this.signer = signer;
     this.proofMarketPlace = ProofMarketPlace__factory.connect(proofMarketPlaceAddress, this.signer);
+    this.rsaRegistry = RsaRegistry__factory.connect(rsaRegistry, this.signer);
   }
 
   public async grantRoleToMatchingEngine(
@@ -28,5 +30,9 @@ export class Admin {
     options?: Overrides
   ): Promise<ContractTransactionResponse> {
     return this.proofMarketPlace.updateEncryptionKey(rsaPub, attestationBytes, { ...options });
+  }
+
+  public async readMatchingEngineKey(): Promise<string> {
+    return this.rsaRegistry.rsa_pub_key(await this.proofMarketPlace.getAddress());
   }
 }
