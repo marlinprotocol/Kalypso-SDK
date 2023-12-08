cbe510f7aa46600eed5dfd38341522840c7a1982
sagarparker <sagar8parker@gmail.com>
2023-12-08 17:12:17 +0530

    Update for the publishGeneratorTest to fetch the generator ecies public key

--- test/publishGeneratorKey.ts
+++ test/publishGeneratorKey.ts
@@ -15,18 +15,38 @@ dotenv.config();
 async function main() {
   try{
     const provider = new ethers.JsonRpcProvider(keys.rpc);
-    const generator_private_key = `${keys.generator_private_key}`;
+    const generator_private_key = keys.generator_private_key;
+    const generator_endpoint = keys.generator_endpoint;
+    const generator_client_api_key = keys.generator_client_api_key;
     const wallet = new ethers.Wallet(generator_private_key, provider);
     console.log("using address", await wallet.getAddress());
   
-    let config = {
+    let attestation_config = {
       method: 'get',
-      url: 'http://65.1.228.145:3000'
+      url: `${generator_endpoint}:1300`
     };
     
-    let attestation_server_response = await axios(config);
+    let attestation_server_response = await axios(attestation_config);
     let attestation_document = attestation_server_response.data;
-  
+
+
+    let data = JSON.stringify({
+      "generator_address": "0xb05e1dA573707223574443AC6DD1054A9e3A451F"
+    });
+
+    let public_key_config = {
+      method: 'get',
+      url: `${generator_endpoint}:5000/api/getGeneratorPublicKeys`,
+      headers: { 
+        'api-key': generator_client_api_key, 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    let generator_public_keys_response = await axios(public_key_config);
+    let ecies_public_key = generator_public_keys_response.data.data.generator_ecies_public_key;
+
     const kalypso = new KalypsoSdk(wallet, kalypsoConfig);
   
     let secret_key: PrivateKey = PrivateKey.fromHex(generator_private_key);
