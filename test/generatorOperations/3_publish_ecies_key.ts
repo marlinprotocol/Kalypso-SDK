8a191b719456bc67a071d44649a278a9aca62328
Akshay Meher <akshay_111meher@hotmail.com>
2024-01-08 14:54:52 +0400

    update generator scripts

--- test/generatorOperations/3_publish_ecies_key.ts
+++ test/generatorOperations/3_publish_ecies_key.ts
@@ -12,22 +12,28 @@ const wallet = new ethers.Wallet(`${keys.generator_private_key}`, provider);
 async function main() {
   console.log("using address", await wallet.getAddress());
 
-  const attestation_verifier_endpoint = keys.attestation_verifier_endpoint;
+  const attestation_verifier_endpoint = "http://65.1.112.107:1400";
 
   const kalypso = new KalypsoSdk(wallet, kalypsoConfig);
 
-  let attestation = await kalypso.Generator().GeneratorEnclaveConnector().getAttestation(attestation_verifier_endpoint);
+  // let attestation = await kalypso.Generator().GeneratorEnclaveConnector().getAttestation(attestation_verifier_endpoint);
+  let attestation = await kalypso
+    .Generator()
+    .GeneratorEnclaveConnector()
+    .getMockAttestation(
+      "0x1f087499fe4b0ce31ad5fe80bd4c03a642e251ed422bd9c08678a97582e8360218cc658dc8ae0cba9147af7273b67377287951c39dd2a2234b4ead1b8dc0fe02"
+    );
 
   console.log("\nAttestation :");
   console.log(attestation);
 
-  let generator_public_keys = await kalypso
-    .Generator()
-    .GeneratorEnclaveConnector()
-    .getGeneratorPublicKeys("0xb05e1dA573707223574443AC6DD1054A9e3A451F");
+  // let generator_public_keys = await kalypso
+  //   .Generator()
+  //   .GeneratorEnclaveConnector()
+  //   .getGeneratorPublicKeys("0xb05e1dA573707223574443AC6DD1054A9e3A451F");
 
-  console.log("\nGenerator public keys :");
-  console.log(generator_public_keys);
+  // console.log("\nGenerator public keys :");
+  // console.log(generator_public_keys);
 
   const tx = await kalypso.Generator().updateEcisKey(attestation.attestation_document);
   const receipt = await tx.wait();
