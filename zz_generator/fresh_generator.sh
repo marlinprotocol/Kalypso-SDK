3c1c4e61ee04bca910a9565909a9103ea47be9ee
Akshay Meher <akshay_111meher@hotmail.com>
2024-03-26 09:42:44 +0400

    update

--- zz_generator/fresh_generator.sh
+++ zz_generator/fresh_generator.sh
@@ -1,5 +1,8 @@
 # yarn test ./test/generatorOperations/16_cancel_all_possible_asks.ts
 yarn test ./test/generatorOperations/generatorEnclave/1_generator_config.ts
 yarn test ./test/generatorOperations/generatorEnclave/2_stake.ts
+
+yarn test ./test/generatorOperations/1_register.ts
+yarn test ./test/generatorOperations/3_join_market_place.ts
 yarn test ./test/generatorOperations/4_update_ecies_key.ts
 yarn test ./test/generatorOperations/generatorEnclave/3_generator_start.ts
