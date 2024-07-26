a6a7ae04991faddfd2f74cfad1486825f1efb9b8
akshay <akshay_111meher@hotmail.com>
2024-07-26 18:33:47 +0400

    remove unwanted console.logs from the inner functions

--- src/types.ts
+++ src/types.ts
@@ -74,24 +74,6 @@ export interface GeneratorConfig {
   supported_markets: string[];
 }
 
-// {
-//   "ws_url": "wss://arb-sepolia.g.alchemy.com/v2/zaEVsF1FaVERyTsM42WGoE4uM7wRyfqK",
-//   "http_url": "https://arb-sepolia.g.alchemy.com/v2/zaEVsF1FaVERyTsM42WGoE4uM7wRyfqK",
-//   "private_key": "91e60908ad659c964169211f07d7c2328ca8919d81dfd772c850bebfd67d4cdf",
-//   "start_block": 16440453,
-//   "chain_id": 421614,
-//   "payment_token": "0x01d84D33CC8636F83d2bb771e184cE57d8356863",
-//   "staking_token": "0xdb69299dDE4A00c99b885D9f8748B2AeD1Fe4Ed4",
-//   "attestation_verifier": "0xDa7a2cBe0a23bb6bF4B3a6cFF0Fba7D0159967A1",
-//   "entity_registry": "0x7376a8729dEEa9BF57e46123A5b74c8419a6Ab62",
-//   "proof_market_place": "0x6F45cb6F85A7fc2C36b18eFA524DE23263Dc2D24",
-//   "generator_registry": "0x41345DA0907ef0633ef420ae8c9D88444622cf81",
-//   "ivs_url":"http://43.205.177.43:3030/checkInput",
-//   "markets":{
-//       "5":"6000",
-//       "1":"7000"
-//   }
-// }
 export interface RuntimeConfig {
   ws_url: string;
   http_url: string;
